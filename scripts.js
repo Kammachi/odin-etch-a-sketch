29fce9b6611aee07fc4b1fff1e6723e60c89d5b7
Kammachi <126817246+Kammachi@users.noreply.github.com>
2024-02-14 22:02:30 +0100

    Add the ability to change the size of the grid

--- scripts.js
+++ scripts.js
@@ -1,17 +1,65 @@
 const container = document.querySelector('.container');
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.classList.add('gridSquare');
 
-    container.appendChild(div);
+function createGrid(numberOfSquares) {
+    for (let i = 0; i < numberOfSquares ** 2; i++) {
+        const squareToAdd = document.createElement('div');
+
+        squareToAdd.classList.add('gridSquare');
+        squareToAdd.style.width = `${100 / numberOfSquares}%`;
+        squareToAdd.style.heigth = `${100 / numberOfSquares}%`;
+
+        container.appendChild(squareToAdd);
+    }
 }
 
 
-const squares = document.querySelectorAll('.gridSquare');
-
-squares.forEach(square => {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = "red";
+function listenForHover() {
+    squares.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = "black";
+        });
     });
+}
+
+
+createGrid(16)
+
+let squares = document.querySelectorAll('.gridSquare');
+
+listenForHover()
+
+
+function removeGrid() {
+    for (let i = 0; i < squares.length; i++) {
+        const squareToRemove = document.querySelector('.gridSquare');
+        container.removeChild(squareToRemove);
+    }
+}
+
+
+const sizeButton = document.querySelector('#sizeButton');
+
+sizeButton.addEventListener('click', () => {
+    const numberOfSquares = parseInt(prompt("Number of squares on the side:"));
+
+    if (isNaN(numberOfSquares)) {
+        alert("Please input a number.");
+        return;
+    }
+
+    if (numberOfSquares <= 0) {
+        alert("Plese input a vaild number.");
+        return;
+    } else if (numberOfSquares > 100) {
+        alert("Your number is too big.");
+        return;
+    }
+
+    removeGrid()
+    createGrid(numberOfSquares)
+
+    squares = document.querySelectorAll('.gridSquare');
+
+    listenForHover()
 });
